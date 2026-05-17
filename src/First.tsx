f1a568cd887c32dd3b3e3c40948c42be17c9de06
Archit Gupta <architg564@gmail.com>
2026-05-17 15:32:24 +0530

    added react and created an component in tsx

---
+++ src/First.tsx
@@ -0,0 +1,10 @@
+const First = () => {
+    return (
+        <>
+            <h1>First Component</h1>
+            <p>This is the first component.</p>
+        </>
+    );
+}
+
+export default First
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export function greet(name: string): string {
-    return `Hello, ${name}!`;
-}
+// export function greet(name: string): string {
+//     return `Hello, ${name}!`;
+// }
 
-export function greet1(name: string): string {
-    return `Hello, kukreja}! ${name}`;
-}
+// export function greet1(name: string): string {
+//     return `Hello, kukreja}! ${name}`;
+// }
